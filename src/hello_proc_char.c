7b6c2f4da4cadb8971c3ce2af1bf0845009da869
spencer <13sfaith@gmail.com>
2023-08-28 10:18:08 -0700

    Fixed copy_to/from_user for correct return value

--- src/hello_proc_char.c
+++ src/hello_proc_char.c
@@ -21,7 +21,7 @@ static int device_release(struct inode *inode, struct file *filp)
 static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
 {
 	char *msg = "Hello pwn-college!\n";
-	return strlen(msg) - copy_to_user(buffer, msg, strlen(msg));
+	return copy_to_user(buffer, msg, strlen(msg)) ? -EFAULT : 0;
 }
 
 static ssize_t device_write(struct file *filp, const char *buf, size_t len, loff_t *off)
